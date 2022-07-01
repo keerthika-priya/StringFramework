27625899f496a50a90c711c2ae57a18942be5ea3
keerthika.priya <keerthika.priya@flipkart.com>
2022-07-01 15:52:08 +0530

    Initial commit

---
+++ StringFramework/String.swift
@@ -0,0 +1,14 @@
+//
+//  String.swift
+//  StringFramework
+//
+//  Created by G Keerthika Priya on 01/07/22.
+//
+
+import Foundation
+
+class StringTest {
+    public static func testThis() {
+        print("Testing")
+    }
+}
---
+++ StringFramework/StringFramework.h
@@ -0,0 +1,18 @@
+//
+//  StringFramework.h
+//  StringFramework
+//
+//  Created by G Keerthika Priya on 01/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StringFramework.
+FOUNDATION_EXPORT double StringFrameworkVersionNumber;
+
+//! Project version string for StringFramework.
+FOUNDATION_EXPORT const unsigned char StringFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringFramework/PublicHeader.h>
+
+
